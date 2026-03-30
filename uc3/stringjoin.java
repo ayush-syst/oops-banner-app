65f895b7b56193778d44f09e3ff33578452616f7
ayush-syst <2389ayush@gmail.com>
2026-03-30 19:19:14 +0530

    updated uc3

--- uc3/stringjoin.java
+++ uc3/stringjoin.java
@@ -1,13 +1,12 @@
 package uc3;
 
 public class stringjoin {
-    public class strjoin {
     public static void main(String[] args) {
         System.out.println(String.join(" ", " OOO  ", " OOO  ", " PPPP  ", "  SSSS "));
         System.out.println(String.join(" ", "O   O ", "O   O ", " P   P ", " S     "));
         System.out.println(String.join(" ", "O   O ", "O   O ", " PPPP  ", "  SSS  "));
         System.out.println(String.join(" ", "O   O ", "O   O ", " P     ", "     S "));
         System.out.println(String.join(" ", " OOO  ", " OOO  ", " P     ", " SSSS  "));
-    }
+    
 }
 }
