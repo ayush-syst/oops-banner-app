087c8bd80ad68003b0c15a1a61dab0b8052ab19b
ayush-syst <2389ayush@gmail.com>
2026-03-31 12:41:30 +0530

    updated uc4

--- uc4/uc4.java
+++ uc4/uc4.java
@@ -1,5 +1,15 @@
 package uc4;
-
 public class uc4 {
-    
+    public static void main(String[] args) {
+        String[] lines = {
+            " OOO   OOO  PPPP   SSSS ",
+            "O   O O   O P   P S     ",
+            "O   O O   O PPPP   SSS  ",
+            "O   O O   O P         S ",
+            " OOO   OOO  P     SSSS  "
+        };
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
 }
