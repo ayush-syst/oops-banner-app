589a280a86171577735e83b142d857a815003dbd
ayush-syst <2389ayush@gmail.com>
2026-03-31 13:03:48 +0530

    created uc5

---
+++ uc5/uc5.java
@@ -0,0 +1,17 @@
+
+public class uc5 {
+    public static void main(String[] args) {
+        String[] lines = {
+            String.join(" ", " OOO   OOO  PPPP   SSSS "),
+            String.join(" ", "O   O O   O P   P S     "),
+            String.join(" ", "O   O O   O PPPP   SSS  "),
+            String.join(" ", "O   O O   O P         S "),
+            String.join(" ", " OOO   OOO  P     SSSS  ")
+        };
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
+    
+
